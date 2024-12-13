ab462ddf7f667baa4551eba39bf27a66977a0d3f
JhonnyARM <angelesjhonny5@gmail.com>
2024-12-13 03:39:46 -0500

    axzaqqqqqqqq

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="login_tests",
-    version="1.1.1",
+    version="1.1.2",
     packages=find_packages(include=["login_tests", "login_tests.*"]),
     install_requires=[
         "Appium-Python-Client==2.0.0",
